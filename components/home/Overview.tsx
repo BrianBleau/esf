c3f1d06900be20cb13a21db4c4e00fd565f21a28
BrianBleau <bleaubrian@gmail.com>
2022-01-04 10:00:32 -0500

    Update Overview.tsx

--- components/home/Overview.tsx
+++ components/home/Overview.tsx
@@ -4,6 +4,10 @@ export default function Overview () {
     return(
         <section className={styles.overviewContainer}>
             <div>
+                <h1>Who We Serve</h1>
+                <p>ESF services are geared towards currently incarcerated felons with a confirmed release date or those who have been recently released. The foundations of success are those who have chosen a field of business, demonstrate a positive attitude, and show a willingness to learn.</p>
+            </div>
+            <div>
                <h1>How We Help</h1>
                <p>
                     Consulting, training services, and support will be an individualized plan for each felon. Their
